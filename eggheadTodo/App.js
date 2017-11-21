7da4978d62eb78f0e165fd9d199d4318873e5d90
slater6 <slaters@gmail.com>
2017-11-20 23:13:22 -0500

    Setup react native project with react-native cli

---
+++ eggheadTodo/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { 
+  View,
+  Text,
+  StyleSheet
+} from 'react-native'
+
+export default class App extends Component {
+  render(){
+    return (
+      <View>
+        <Text>Hi Bryab</Text>
+      </View>
+    )
+  }
+}
