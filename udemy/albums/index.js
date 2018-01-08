7f919a7da6ae6257422aab7f4cbaeae2df3e500e
Bryan Rodrigues <brodrigues@weareinteractive.ca>
2018-01-08 13:14:55 -0500

    Styled Header Component

--- udemy/albums/index.js
+++ udemy/albums/index.js
@@ -5,7 +5,7 @@ import {
 import Header from './src/components/header';
 
 const App = () => (
-  <Header />
+  <Header headerText="Albums!" />
 );
 
 AppRegistry.registerComponent('albums', () => App);
--- udemy/albums/src/components/header.js
+++ udemy/albums/src/components/header.js
@@ -1,7 +1,38 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
-const Header = () => <Text>Albums!</Text>;
+const styles = {
+  viewStyle: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F8F8F8',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.2,
+    elevation: 2,
+    position: 'relative',
+  },
+  textStyle: {
+    fontSize: 20,
+  },
+};
+
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>
+  );
+};
+
 
 export default Header;
 
