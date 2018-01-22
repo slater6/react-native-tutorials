f87be91c9478d403199bcb8fd66a8d1e67da1a16
Bryan Rodrigues <brodrigues@weareinteractive.ca>
2018-01-22 13:04:16 -0500

    Setup Card Component

--- udemy/albums/src/components/AlbumList.js
+++ udemy/albums/src/components/AlbumList.js
@@ -2,20 +2,30 @@ import React, { PureComponent } from 'react';
 import { Text, View } from 'react-native';
 import axios from 'axios';
 
+import AlbumDetail from './AlbumDetail';
+
 class AlbumList extends PureComponent {
+  state = { albums: [] };
+
   async componentWillMount() {
-    const result = await axios.get('https://rallycoding.herokuapp.com/api/music_albums');
-    console.log(result);
+    const result = await axios.get(
+      'https://rallycoding.herokuapp.com/api/music_albums'
+    );
+
+    this.setState({ albums: result.data });
+
+    console.log(this.state.albums);
   }
 
+  renderAlbums = () => {
+    return this.state.albums.map(album => {
+      return <AlbumDetail key={album.title} album={album} />;
+    });
+  };
+
   render() {
-    return (
-      <View>
-        <Text />
-      </View>
-    );
+    return <View>{this.renderAlbums()}</View>;
   }
 }
 
 export default AlbumList;
-
