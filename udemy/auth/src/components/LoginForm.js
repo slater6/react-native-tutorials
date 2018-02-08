36c74fad3e5d547783f1e9aed6827dd59c7ea97e
Bryan Rodrigues <slaters@gmail.com>
2018-02-08 13:27:41 -0500

    Added firebase auth -- tried to implement await

--- udemy/auth/src/components/LoginForm.js
+++ udemy/auth/src/components/LoginForm.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';
+import { Text } from 'react-native';
 import { Card, CardSection, Button, Input } from './common';
 import firebase from 'firebase';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = { email: '', password: '', error: '' };
 
-  onButtonPress = () => {
+  onButtonPress = async () => {
     const { email, password } = this.state;
-    firebase.auth().signInWithEmailAndPassword(email, password);
+
+    this.setState({
+      error: 'Checking Authentication..'
+    });
+
+    const loginAttempt = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
+
+    if (!loginAttempt) {
+      const createUser = await firebase
+        .auth()
+        .createUserWithEmailAndPassword(email, password);
+
+      if (!createUser) {
+        this.setState({
+          error: 'Authentication Failed.'
+        });
+        return;
+      }
+
+      this.setState({
+        error: 'New Account Created!'
+      });
+      return;
+    }
+
+    this.setState({
+      error: 'You Have Been Authenticated!'
+    });
   };
 
   render() {
@@ -28,6 +58,7 @@ class LoginForm extends Component {
             onChangeText={password => this.setState({ password })}
           />
         </CardSection>
+        <Text style={styles.errorTextStyle}>{this.state.error}</Text>
         <CardSection>
           <Button onPress={this.onButtonPress} title="Login" />
         </CardSection>
@@ -35,5 +66,13 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
