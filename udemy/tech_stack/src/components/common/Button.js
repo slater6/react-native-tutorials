c8acca00717b5aac8c4a51e507b4856f516850a9
Bryan Rodrigues <brodrigues@weareinteractive.ca>
2018-04-25 13:31:31 -0400

    Added New Tect Stack Tutorial

---
+++ udemy/tech_stack/src/components/common/Button.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const styles = {
+  textStyle: {
+    alignSelf: 'center',
+    color: '#007aff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#007aff',
+    marginLeft: 5,
+    marginRight: 5
+  }
+};
+
+const Button = props => (
+  <TouchableOpacity onPress={props.onPress} style={styles.buttonStyle}>
+    <Text style={styles.textStyle}>{props.title}</Text>
+  </TouchableOpacity>
+);
+
+export { Button };
