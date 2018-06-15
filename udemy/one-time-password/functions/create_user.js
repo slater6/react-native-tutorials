a448f164d5d39254b15a70657c6789ccbf3deace
unknown <slaters@gmail.com>
2018-06-15 06:51:24 -0400

    Added real create user functionality

--- udemy/one-time-password/functions/create_user.js
+++ udemy/one-time-password/functions/create_user.js
@@ -1,3 +1,21 @@
-module.exports = function (req, res) {
-    res.send(req.body)
-}
+const admin = require('firebase-admin');
+
+module.exports = function(req, res) {
+  if (!req.body.phone) {
+    return res.status(422).send({
+      error: 'Bad Input'
+    });
+  }
+
+  const phone = String(req.body.phone).replace(/[^\d]/g, '');
+
+  return admin
+    .auth()
+    .createUser({ uid: phone })
+    .then(user => {
+      return res.send(user);
+    })
+    .catch(err => {
+      return res.status(422).send({ error: err });
+    });
+};
--- udemy/one-time-password/functions/index.js
+++ udemy/one-time-password/functions/index.js
@@ -1,11 +1,11 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
-const createUser = require('./create_user')
+const createUser = require('./create_user');
+const serviceAccount = require('./key/one-time-password-account-key.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://one-time-password-c0c97.firebaseio.com'
 });
 
 exports.createUser = functions.https.onRequest(createUser);
